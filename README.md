c34557a28020c79d7a87fd8c446811f3f4da2dfe
Huaqing Xu <huaqingxu89@gmail.com>
2023-03-21 00:06:39 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # rf_stocks
+```sh
+pipenv --python 3.9.12    # rf_stocks-kw8jdFWM
+pipenv run python -m ipykernel install --name rf_stocks-kw8jdFWM
+```
+
+## 基于随机森林算法的股价涨跌应用研究
+
+I. 绪论
+A. 研究背景
+B. 研究目的和意义
+C. 研究内容
+D. 研究方法
+
+II. 随机森林算法介绍
+A. 决策树算法概述
+B. 随机森林算法原理
+C. 随机森林算法优缺点
+
+III. 股价涨跌预测模型构建
+A. 数据收集和预处理
+B. 特征工程
+C. 模型训练和优化
+D. 模型评估和比较
+
+IV. 实验结果与分析
+A. 实验设置
+B. 实验结果
+C. 结果分析和讨论
+
+V. 结论与展望
+A. 主要研究结论
+B. 研究不足与展望
+C. 后续研究方向
+
+VI. 参考文献
--- rf_stocks.py
+++ rf_stocks.py
@@ -1,60 +1,9 @@
 import yfinance as yf
-import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+
 
 # 下载股票数据并保存到本地文件
 def download_data(ticker):
     data = yf.download(ticker, start='2010-01-01', end='2022-03-08')
     data.to_csv(f'{ticker}.csv')
     return data
-
-# 从本地文件中载入股票数据
-def load_data(ticker):
-    data = pd.read_csv(f'{ticker}.csv', index_col='Date', parse_dates=True)
-    return data
-
-# 获取股票数据
-def get_data(ticker):
-    try:
-        data = load_data(ticker)
-    except FileNotFoundError:
-        data = download_data(ticker)
-    return data
-
-# 计算技术指标
-def calculate_technical_indicators(data):
-    data['SMA'] = data['Adj Close'].rolling(window=20).mean()
-    # data['RSI'] = talib.RSI(data['Adj Close'], timeperiod=14)
-    data.dropna(inplace=True)
-    return data
-
-# 选股
-def select_stocks(ticker):
-    data = get_data(ticker)
-    data = calculate_technical_indicators(data)
-
-    # 构建特征矩阵和标签向量
-    # features = data[['SMA', 'RSI']]
-    features = data[['SMA']]
-    labels = (data['Adj Close'].shift(-1) > data['Adj Close']).astype(int)
-
-    # 划分训练集和测试集
-    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, shuffle=False)
-
-    # 构建随机森林模型
-    clf = RandomForestClassifier(n_estimators=100)
-    clf.fit(X_train, y_train)
-
-    # 预测测试集结果
-    y_pred = clf.predict(X_test)
-
-    # 计算准确率
-    accuracy = clf.score(X_test, y_test)
-
-    return accuracy
-
-if __name__ == "__main__":
-    acc = select_stocks("BABA")
-    print(acc)
 
