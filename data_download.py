9b7a138958f51f015030808af43aec454bab1167
Huaqing Xu <huaqingxu89@gmail.com>
2023-03-23 21:12:29 +0800

    update

--- data_download.py
+++ data_download.py
@@ -1,14 +1,14 @@
 import yfinance as yf
 
+
 # 下载股票数据并保存到本地文件
 def download_data(ticker):
     data = yf.download(ticker, start='2010-01-01', end='2022-03-21')
-    data.to_csv(f'{ticker}.csv')
+    data.to_csv(f'data/{ticker}.csv')
     return data
 
 if __name__ == "__main__":
     download_data("000858.SZ") # 五粮液
-    download_data("603259") # 药明康德
-    download_data("000921") # 海信家电
+    download_data("603259.SS") # 药明康德
 
 
--- rf_stocks.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-
-
-# 下载股票数据并保存到本地文件
-def download_data(ticker):
-    data = yf.download(ticker, start='2010-01-01', end='2022-03-08')
-    data.to_csv(f'{ticker}.csv')
-    return data
-
